aa170db0cd98a4964db7de5cfd200544697d98ef
alsh0221 <alsh0221@netcracker.com>
2021-11-07 21:09:31 +0300

    Second Layout

--- js/index.js
+++ js/index.js
@@ -17,6 +17,7 @@ let swiper = new Swiper(".testimonials-slider", {
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
+      dynamicBullets: true,
     },
     breakpoints: {
         992: {
